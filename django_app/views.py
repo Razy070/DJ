31246f062fa5b8d5ec94c7b3ba22b898f90f8506
Razy070 <rdimovin@mail.ru>
2022-12-05 22:34:47 +0700

    up

--- django_app/views.py
+++ django_app/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpRequest
 
 
-# from django_app import models as django_models
+# Create your views here.
 
 
-def index(request):
-    # generator1 = ({"id": random.randint(0, x), "name": f"Bogdan {x}", "age": x + 20} for x in range(1, 100))
+def home(request):
+    return render(request, 'django_app/home.html')
 
-    data = {"header": "Hello Django", "message": "Welcome to Python"}
-    return render(request, 'django_app/home.html', context=data)
 
+def my_api_one(request: HttpRequest, post_id: int) -> JsonResponse:
+    datas = [
+        {
+            "userId": int(round(post_id / 10, 0)),
+            "id": post_id,
+        }
+    ]
+
+    return JsonResponse(data=datas, safe=False)
+
+
+def my_api(request):
+    datas = [
+        {
+            "userId": round(x / 10, 0),
+            "id": x,
+        }
+        for x in range(1, 100 + 1)
+    ]
+    return JsonResponse(data=datas, safe=False)
