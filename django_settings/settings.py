4e4a585ac014a5e1b5621da52e0550823d27c349
Razy070 <rdimovin@mail.ru>
2022-12-05 22:17:24 +0700

    up

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4uthox(pbii-73k$1t!r3$d%a-=^-)y4zf601t(4+r$(x_-s(!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
